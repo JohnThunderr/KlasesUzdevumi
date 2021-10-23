6b1ffe27eb1609528ba313d03b3ba58bf0854bd1
JohnThunderr <j.thunder.original@gmail.com>
2021-10-23 13:48:19 +0300

    Code beautification

--- src/Casting/Main.java
+++ src/Casting/Main.java
@@ -10,29 +10,32 @@ public class Main {
 
         Random r = new Random();
         Scanner sc = new Scanner(System.in);
-        String[] izvēles = {"Akmens" , "Sķēres" , "Papīrs"};
+        String[] options = {"Akmens" , "Sķēres" , "Papīrs"};
         int randomCPUPick = r.nextInt(3);
 
-        System.out.println("Lūdzu izvēlieties Akmeni, Šķēres vai Papīru nospiežot attiecīgo ciparu uz klaviaturās:" +
-                "\n1] Akmens" + "\n2] Sķēres" + "\n3] Papīrs");
+        System.out.println("""
+                Lūdzu izvēlieties Akmeni, Šķēres vai Papīru nospiežot attiecīgo ciparu uz klaviaturās:
+                1] Akmens
+                2] Sķēres
+                3] Papīrs""");
         int playerPick = sc.nextInt();
 
         if((playerPick >= 1) && (playerPick <= 3))
-            System.out.println("Spēlētājs izvēlējās: " + izvēles[playerPick-1]);
+            System.out.println("Spēlētājs izvēlējās: " + options[playerPick-1]);
         else{
             System.out.println("Izvēle nav pieejama!");
             System.exit(playerPick);
         }
 
         if ((playerPick == 1) && (randomCPUPick == 0) || (playerPick == 2) && (randomCPUPick == 1) || (playerPick == 3) && (randomCPUPick == 2))
-            System.out.println("Neizšķirts!! \n Spēlētājs izvēļējās " + izvēles[playerPick-1]
-                    + " un Dators izvēlējās " + izvēles[randomCPUPick]);
+            System.out.println("Neizšķirts!! \n Spēlētājs izvēļējās " + options[playerPick-1]
+                    + " un Dators izvēlējās " + options[randomCPUPick]);
         else if ((playerPick == 1) && (randomCPUPick == 2) || (playerPick == 2) && (randomCPUPick == 0) || (playerPick == 3) && (randomCPUPick == 1))
-            System.out.println("Uzvar spēlētājs!! \n Spēlētājs izvēļējās " + izvēles[playerPick-1]
-                    + " un Dators izvēlējās " + izvēles[randomCPUPick]);
+            System.out.println("Uzvar spēlētājs!! \n Spēlētājs izvēļējās " + options[playerPick-1]
+                    + " un Dators izvēlējās " + options[randomCPUPick]);
         else if ((playerPick == 1) && (randomCPUPick == 1) || (playerPick == 2) && (randomCPUPick == 2) || (playerPick == 3) && (randomCPUPick == 0))
-            System.out.println("Uzvar Dators!! \n Spēlētājs izvēļējās " + izvēles[playerPick-1]
-                    + " un Dators izvēlējās " + izvēles[randomCPUPick]);
+            System.out.println("Uzvar Dators!! \n Spēlētājs izvēļējās " + options[playerPick-1]
+                    + " un Dators izvēlējās " + options[randomCPUPick]);
         else
             System.out.println("Error in logic, this was not supposed to happen");
 
